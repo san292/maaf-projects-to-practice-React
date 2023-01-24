fc81c85c64b0f3f8b225449396e4edaeb53d29ad
San292 <79156846+san292@users.noreply.github.com>
2023-01-24 02:46:36 +0100

    Delete cocktail directory

--- cocktail/apiplaceholder/src/api/cusetmfetch.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-export const customfetch = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com/posts'
-});
--- cocktail/apiplaceholder/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import DetailsPage from './DetailsPage';
-import HomePage from './HomePage';
-import Favoris from './Favoris';
-
-export { DetailsPage, HomePage, Favoris };
